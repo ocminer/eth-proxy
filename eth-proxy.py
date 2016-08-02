9ea25e9fee747f6ca8e34f16440740b8ba2ca022
ocminer <m@rcel.to>
2016-08-02 08:59:57 +0200

    Not needed

--- eth-proxy.py
+++ eth-proxy.py
@@ -11,9 +11,9 @@ import stratum.logger
 log = stratum.logger.get_logger('proxy')
 
 if __name__ == '__main__':
-    if len(settings.WALLET)!=42 and len(settings.WALLET)!=40:
-        log.error("Wrong WALLET!")
-        sys.exit()
+#    if len(settings.WALLET)!=42 and len(settings.WALLET)!=40:
+#        log.error("Wrong WALLET!")
+#        sys.exit()
     settings.CUSTOM_EMAIL = settings.MONITORING_EMAIL if settings.MONITORING_EMAIL and settings.MONITORING else ""
 
 from twisted.internet import reactor, defer, protocol
